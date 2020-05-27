b031b8ce80b79f7a018ed9a7b8e07d9c322356a3
ClaencyF <56391107+ClaencyF@users.noreply.github.com>
2020-05-27 22:41:34 +0200

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { InterceptorService } from './interceptor.service';
+import { ExternalApiComponent } from './external-api/external-api.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'external-api',
+    component: ExternalApiComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: InterceptorService,
+      multi: true
+    }
+  ]
+ 
 })
 export class AppRoutingModule { }
